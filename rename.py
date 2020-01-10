c35557b9624ea6bc2858fce3f3867c8297e38e67
Alex Anderson <alex@alwian.com>
2020-01-10 15:27:14 -0400

    Added the option to rename directories

--- rename.py
+++ rename.py
@@ -3,16 +3,16 @@ import os
 import re
 
 
-def rename_file(file_path):
-    """Rename a file using patterns provided via the command line."""
+def rename_item(path):
+    """Rename a file or directory using patterns provided via the command line."""
 
     # Extract file information
-    filename = os.path.basename(file_path)
-    file_extension = filename.split('.', maxsplit=1)[-1]
-    directory = os.path.dirname(file_path)
+    item_name = os.path.basename(path)
+    item_extension = item_name.split('.', maxsplit=1)[-1]
+    item_root = os.path.dirname(path)
 
     # Find groups specified by the user
-    initial_groups = re.search(args.initial, filename)
+    initial_groups = re.search(args.initial, item_name)
 
     # If groups found, get dict of groups so values can be retrieved
     if initial_groups is not None:
@@ -31,11 +31,11 @@ def rename_file(file_path):
         result = result.replace(group, initial_groups[group.replace('[', '').replace(']', '')])
 
     # If file had an extension, add it back on
-    if file_extension != filename:
-        result = result + '.' + file_extension
+    if item_extension != item_name:
+        result = result + '.' + item_extension
 
     # Rename the file
-    os.rename(file_path, os.path.join(directory, result))
+    os.rename(path, os.path.join(item_root, result))
 
 
 if __name__ == "__main__":
@@ -44,10 +44,13 @@ if __name__ == "__main__":
         description='Bulk rename files using regex',
         epilog='Report any issues at - https://github.com/alwian/py-rename/issues'
     )
-    parser.add_argument('-s', '--subdirectories', action='store_true', help='rename files inside of subdirectories')
-    parser.add_argument('directory', help="the directory containing files to be renamed", )
-    parser.add_argument("initial", help="the initial regex pattern of all files to be renamed")
-    parser.add_argument("desired", help="the desired  pattern of all files to be renamed")
+    rename_type = parser.add_mutually_exclusive_group(required=True)
+    rename_type.add_argument('-f', '--files', action='store_true', help='rename files')
+    rename_type.add_argument('-d', '--directories', action='store_true', help='rename directories')
+    parser.add_argument('-s', '--subdirectories', action='store_true', help='rename items inside of subdirectories')
+    parser.add_argument('directory', help="the directory containing items to be renamed", )
+    parser.add_argument("initial", help="the initial regex pattern of all items to be renamed")
+    parser.add_argument("desired", help="the desired  pattern of all items to be renamed")
     args = parser.parse_args()
 
     # Check provided directory is valid
@@ -57,9 +60,20 @@ if __name__ == "__main__":
     # If subdirectories flag specified
     if args.subdirectories:
         for root, dirs, files in os.walk(args.directory):
-            for file in files:
-                rename_file(os.path.join(root, file))
+            # Rename files
+            if args.files:
+                for file in files:
+                    rename_item(os.path.join(root, file))
+            # Rename directories
+            elif args.directories:
+                for directory in dirs:
+                    rename_item(os.path.join(root, directory))
     # If only renaming inside of specified directory
     else:
-        for file in os.listdir(args.directory):
-            rename_file(os.path.join(args.directory, file))
+        for item in os.listdir(args.directory):
+            # Rename files
+            if args.files and os.path.isfile(os.path.join(args.directory, item)):
+                rename_item(os.path.join(args.directory, item))
+            # Rename directories
+            elif args.directories and os.path.isdir(os.path.join(args.directory, item)):
+                rename_item(os.path.join(args.directory, item))
